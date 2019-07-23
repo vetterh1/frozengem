5b000de76e5081dd909e5636464fc8d0ab1e050e
Laurent VETTERHOEFFER <vetterh1@yahoo.fr>
2019-07-23 16:49:12 +0200

    orientation fix , to test !

--- src/pages/utils/PictureSelection.jsx
+++ src/pages/utils/PictureSelection.jsx
@@ -24,77 +24,79 @@ const styles = theme => ({
 
     const resizePicture = async (img, MAX_WIDTH = 800, MAX_HEIGHT = 800) => {
 
-        var canvas = document.createElement("canvas");
+      var canvas = document.createElement("canvas");
 
-        const exifTags = await getExifTagsAsync(img);
-        console.log(`resizePicture: exifTags=`, exifTags);
+      const exifTags = await getExifTagsAsync(img);
+      console.log(`resizePicture: exifTags=`, exifTags);
 
-        const orientation = exifTags.Orientation;
-        if ([5, 6, 7, 8].indexOf(orientation) > -1) {
-            canvas.width = img.height;
-            canvas.height = img.width;
-        } else {
-            canvas.width = img.width;
-            canvas.height = img.height;
-        }
+      const orientation = exifTags.Orientation;
+      let turn = ([5, 6, 7, 8].indexOf(orientation) > -1);
+      let w = img.width;
+      let h = img.height;
+      if(turn) {
+          w = img.height;
+          h = img.width;
+      }
 
-
-        const msg1 = `resizePicture: orientation=${orientation} width=${img.width}, height=${img.height}`;
-        console.log(msg1);
-        alert(msg1);
-
-
-        if (canvas.width > canvas.height) {
-            if (canvas.width > MAX_WIDTH) {
-              canvas.height *= MAX_WIDTH / canvas.width;
-              canvas.width = MAX_WIDTH;
-            }
-        } else {
-            if (canvas.height > MAX_HEIGHT) {
-              canvas.width *= MAX_HEIGHT / canvas.height;
-              canvas.height = MAX_HEIGHT;
-            }
-        }
-
-
-        const msg2 = `resizePicture: to... width=${canvas.width}, height=${canvas.height}`;
-        console.log(msg2);
-        alert(msg2);
-
-        var ctx = canvas.getContext("2d");
-
-
-        switch (orientation) {
-            case 2:
-              ctx.transform(-1, 0, 0, 1, canvas.width, 0);
-              break;
-            case 3:
-              ctx.transform(-1, 0, 0, -1, canvas.width, canvas.height);
-              break;
-            case 4:
-              ctx.transform(1, 0, 0, -1, 0, canvas.height);
-              break;
-            case 5:
-              ctx.transform(0, 1, 1, 0, 0, 0);
-              break;
-            case 6:
-              ctx.transform(0, 1, -1, 0, canvas.height, 0);
-              break;
-            case 7:
-              ctx.transform(0, -1, -1, 0, canvas.height, canvas.width);
-              break;
-            case 8:
-              ctx.transform(0, -1, 1, 0, 0, canvas.width);
-              break;
-            default:
-              ctx.transform(1, 0, 0, 1, 0, 0);
+      if (w > h) {
+          if (w > MAX_WIDTH) {
+            h *= MAX_WIDTH / w;
+            w = MAX_WIDTH;
           }
-    
-          ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      } else {
+          if (h > MAX_HEIGHT) {
+            w *= MAX_HEIGHT / h;
+            h = MAX_HEIGHT;
+          }
+      }
+
+      canvas.width = w;
+      canvas.height = h;
+
+      const msg2 = `resizePicture: to... w=${w}, h=${h}, o=${orientation}`;
+      console.log(msg2);
+      // alert(msg2);
+
+      var ctx = canvas.getContext("2d");
 
 
-        return await canvasToBlobAsync(canvas);
-    }
+      switch (orientation) {
+          case 2:
+            ctx.transform(-1, 0, 0, 1, canvas.width, 0);
+            break;
+          case 3:
+            ctx.transform(-1, 0, 0, -1, canvas.width, canvas.height);
+            break;
+          case 4:
+            ctx.transform(1, 0, 0, -1, 0, canvas.height);
+            break;
+          case 5:
+            ctx.transform(0, 1, 1, 0, 0, 0);
+            break;
+          case 6:
+            ctx.transform(0, 1, -1, 0, canvas.width, 0);
+            break;
+          case 7:
+            ctx.transform(0, -1, -1, 0, canvas.height, canvas.width);
+            break;
+          case 8:
+            ctx.transform(0, -1, 1, 0, 0, canvas.width);
+            break;
+          default:
+            ctx.transform(1, 0, 0, 1, 0, 0);
+        }
+  
+        ctx.drawImage(img, 0, 0, turn ? h : w, turn ? w : h);
+
+
+      return await canvasToBlobAsync(canvas);
+  }
+
+
+
+
+
+
 
 
     const onInputChange = async (e) => {
