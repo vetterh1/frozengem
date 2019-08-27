80d3156c229345c4e9d2c37f1f3c390ce5923995
Laurent VETTERHOEFFER <vetterh1@yahoo.fr>
2019-08-27 11:53:16 +0200

    sh wip

--- make_backup_downloadable.sh
+++ make_backup_downloadable.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 
-echo 
-echo 
-echo 
 echo -----------------------   Make backup downloadable from browser  -----------------------
-echo 
-echo       To be run from Mongo APP server 51.255.46.214
-echo 
-echo       Actions: 
-echo       - Copy latest files and db backup to public folder
-echo 
-echo       Results: they are available here:
-echo       - https://frozengem.com/public-backup/files.tar.gz
-echo       - https://frozengem.com/public-backup/db.tar.gz
-echo
-echo       Should put this script in crontab for automatic running
-echo       but the running time should be after the backup time: 8, 18
-echo "         0 7,19 * * * /home/lve/make_backup_downloadable.sh &>> /home/lve/make_backup_downloadable.log "
-echo "         @reboot sleep 160 && /home/lve/make_backup_downloadable.sh &>> /home/lve/make_backup_downloadable.log "
-echo 
-echo ---------------------------- Prepares Backup ----------------------------
-echo 
-echo 
+# 
+#       To be run from Mongo APP server 51.255.46.214
+# 
+#       Actions: 
+#       - Copy latest db and files from the backup folder to a folder publicly available through the FO server
+# 
+#       Results: they are available here:
+#       - https://frozengem.com/public-backup/files.tar.gz
+#       - https://frozengem.com/public-backup/db.tar.gz
+#
+#       Should put this script in crontab for automatic running
+#       but the running time should be after the backup time: 8, 18
+# "         0 7,19 * * * /home/lve/make_backup_downloadable.sh &>> /home/lve/make_backup_downloadable.log "
+# "         @reboot sleep 160 && /home/lve/make_backup_downloadable.sh &>> /home/lve/make_backup_downloadable.log "
+# 
 
-
+echo Create folder if does not exist yet. Generates an OK error if it already exists.
 mkdir ~/workspace/frozengem/build/frozenbackup
-cp -p "`ls -dftr1 ~/frozengem_files_backup/* | tail -1`" ~/workspace/frozengem/build/frozenbackup/files.tar.gz
-cp -p "`ls -dftr1 ~/frozengem_db_backup/* | tail -1`" ~/workspace/frozengem/build/frozenbackup/db.tar.gz
+
+echo Copy latest db and files from the backup folder to a folder publicly available through the FO server
+cp -p "`ls -frt1 ~/frozengem_files_backup/* | tail -n 1`" ~/workspace/frozengem/build/frozenbackup/files.tar.gz
+cp -p "`ls -frt1 ~/frozengem_db_backup/* | tail -n 1`" ~/workspace/frozengem/build/frozenbackup/db.tar.gz
+
+echo Content of the public folder:
+ls -flrth  ~/workspace/frozengem/build/frozenbackup
+
+# notes on ls options:
+# -f: remove colore
+# -rt: sort by reverse time --> newest last 
+# -1: 1 result per line
+# -h: (with -l): human readable --> kb, mb...
+
+# notes on tail options:
+# -n 1: displays only the last line
+
