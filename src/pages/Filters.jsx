078df55fe11106ad2589500fa1e57c10193af1ff
Laurent VETTERHOEFFER <vetterh1@yahoo.fr>
2020-01-15 08:50:51 +0100

    minor fixes

--- src/pages/Filters.jsx
+++ src/pages/Filters.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */ 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { connect } from 'react-redux';
 import { itemsFilterActions } from '../_actions/itemsFilterActions';
 import { injectIntl } from "react-intl";
