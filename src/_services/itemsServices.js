8aded6ec9f782a36859f9cf9d6e7c3146f9b3cec
laurent <vetterh1@yahoo.fr>
2020-02-04 09:58:21 +0100

    prepare for complete / not complete notification with _isComplete

--- src/_services/itemsServices.js
+++ src/_services/itemsServices.js
@@ -1,18 +1,15 @@
-import qs from 'qs';
-import axios from 'axios';
-import config from '../data/config'
-import {Months} from '../i18n/i18nDates';
+import qs from "qs";
+import axios from "axios";
+import config from "../data/config";
+import { Months } from "../i18n/i18nDates";
 import { characteristicsServices } from "./characteristicsServices";
 
-  
 const ExpirationLevel = {
   EXPIRATION_PASSED: 0,
   EXPIRATION_NEXT_30_DAYS: 1,
   EXPIRATION_WITHIN_3_MONTHS: 2,
-  EXPIRATION_LATER: 3,
+  EXPIRATION_LATER: 3
 };
-
-
 
 export const itemsServices = {
   computeItemUtilityFields,
@@ -25,21 +22,27 @@ export const itemsServices = {
   ExpirationLevel
 };
 
-
-
 function _computeExpirationLevel(dateInMs) {
   const nowInMs = Date.now();
   const oneMonthInMs = 1 * 30 * 24 * 60 * 60 * 1000;
-  if( dateInMs < nowInMs ) return ExpirationLevel.EXPIRATION_PASSED;
-  if( dateInMs < nowInMs + 1 * oneMonthInMs ) return ExpirationLevel.EXPIRATION_NEXT_30_DAYS;
-  if( dateInMs < nowInMs + 3 * oneMonthInMs ) return ExpirationLevel.EXPIRATION_WITHIN_3_MONTHS;
+  if (dateInMs < nowInMs) return ExpirationLevel.EXPIRATION_PASSED;
+  if (dateInMs < nowInMs + 1 * oneMonthInMs)
+    return ExpirationLevel.EXPIRATION_NEXT_30_DAYS;
+  if (dateInMs < nowInMs + 3 * oneMonthInMs)
+    return ExpirationLevel.EXPIRATION_WITHIN_3_MONTHS;
   return ExpirationLevel.EXPIRATION_LATER;
 }
 
 
+function _isComplete(item) {
+  let isComplete = true;
+  return isComplete;
+}
 
-  /*
+
+/*
       Adds:
+      __isComplete
       __detailsArray
       __expirationLevel
       __avatarBackgroundColor
@@ -59,42 +62,38 @@ function _computeExpirationLevel(dateInMs) {
       __monthExpirationAsText
   */
 
-//  function computeItemUtilityFields(item, language, theme) {
-
-
 function computeItemUtilityFields(item, language, characteristics) {
-
-  // NOT PURE !!!  
+  // NOT PURE !!!
   // Changes item
 
   item.__detailsArray = item.details ? item.details.split(",") : [];
   item.__expirationLevel = _computeExpirationLevel(item.expirationDate);
   switch (item.__expirationLevel) {
     case ExpirationLevel.EXPIRATION_PASSED:
-      item.__avatarBackgroundColor = 'expired';
-      item.__cardBackgroundColor = 'expired';
+      item.__avatarBackgroundColor = "expired";
+      item.__cardBackgroundColor = "expired";
       // item.__iconExpiration = <PanToolIcon />;
-      item.__expirationText = {id: 'expiration.message.passed'};
+      item.__expirationText = { id: "expiration.message.passed" };
       break;
     case ExpirationLevel.EXPIRATION_NEXT_30_DAYS:
-      item.__avatarBackgroundColor = 'next_30_days';
-      item.__cardBackgroundColor = 'next_30_days';
+      item.__avatarBackgroundColor = "next_30_days";
+      item.__cardBackgroundColor = "next_30_days";
       // item.__iconExpiration = <PriorityHighIcon />;
-      item.__expirationText = {id: 'expiration.message.next_30_days'};
+      item.__expirationText = { id: "expiration.message.next_30_days" };
       break;
     case ExpirationLevel.EXPIRATION_WITHIN_3_MONTHS:
-      item.__avatarBackgroundColor = 'within_3_months';
-      item.__cardBackgroundColor = 'within_3_months';
+      item.__avatarBackgroundColor = "within_3_months";
+      item.__cardBackgroundColor = "within_3_months";
       // item.__iconExpiration = <TimerIcon />;
-      item.__expirationText = {id: 'expiration.message.within_3_months'};
+      item.__expirationText = { id: "expiration.message.within_3_months" };
       break;
     default:
-      item.__avatarBackgroundColor = 'later';
-      item.__cardBackgroundColor = 'later';
+      item.__avatarBackgroundColor = "later";
+      item.__cardBackgroundColor = "later";
       // item.__iconExpiration = <DoneIcon />;
-      item.__expirationText = {id: 'expiration.message.later'};
+      item.__expirationText = { id: "expiration.message.later" };
       break;
-  } 
+  }
 
   const rawFields = {
     category: item.category,
@@ -103,182 +102,191 @@ function computeItemUtilityFields(item, language, characteristics) {
     freezer: item.freezer,
     location: item.location,
     size: item.size,
-    detailsArray: item.__detailsArray,     
-  }
+    detailsArray: item.__detailsArray
+  };
 
-  const characteristicsUtilityFields = characteristicsServices.getCharacteristicsUtilityFields(rawFields, language, characteristics);
+  const characteristicsUtilityFields = characteristicsServices.getCharacteristicsUtilityFields(
+    rawFields,
+    language,
+    characteristics
+  );
 
   item.__categoryText = characteristicsUtilityFields.category;
   item.__containerText = characteristicsUtilityFields.container;
   item.__colorText = characteristicsUtilityFields.color;
   item.__freezerText = characteristicsUtilityFields.freezer;
   item.__locationText = characteristicsUtilityFields.location;
-  item.__nameOrCategory = item.name && item.name.length > 0 ? item.name : item.__categoryText;
-  item.__sizeInText = item.removed ? '-' : characteristicsUtilityFields.size;
+  item.__nameOrCategory =
+    item.name && item.name.length > 0 ? item.name : item.__categoryText;
+  item.__sizeInText = item.removed ? "-" : characteristicsUtilityFields.size;
   const detailsNamesArray = characteristicsUtilityFields.detailsArray;
-  item.__detailsNames = detailsNamesArray ? detailsNamesArray.join( ', ') : null;
+  item.__detailsNames = detailsNamesArray ? detailsNamesArray.join(", ") : null;
   item.__imageExists = item.pictureName || item.thumbnailName;
 
   const expirationAsDate = new Date(item.expirationDate);
   item.__yearExpiration = expirationAsDate.getFullYear();
   const monthExpiration = expirationAsDate.getMonth();
   item.__monthExpirationAsText = Months[language][monthExpiration];
+
+  item.__isComplete = _isComplete(item);
 }
-  
+
+
 
 
 function computeAllItemsUtilityFields(items, language, characteristics) {
-
-  // NOT PURE !!!  
+  // NOT PURE !!!
   // Changes items
 
-  items.forEach( item => computeItemUtilityFields(item, language, characteristics) );
+  items.forEach(item =>
+    computeItemUtilityFields(item, language, characteristics)
+  );
   return items;
 }
-
-
-          
 
 //
 // Fetch items from Server
 //
 
+async function fetchItemsFromServer(user, characteristics, removed = false) {
+  console.debug(
+    "|--- SERVER CALL ---|--- GET ---| itemsServices.fetchItemsFromServer"
+  );
 
-  async function fetchItemsFromServer(user, characteristics, removed = false) {
-    console.debug('|--- SERVER CALL ---|--- GET ---| itemsServices.fetchItemsFromServer');
- 
-    // No token, no fetchItemsFromServer!
-    const token = localStorage.getItem('accessToken');
-    if(!token) throw new Error({ error: "no token!" });
-    if(!user) throw new Error({ error: "no user!" });
-  
-    const params = { 'access_token': token, 'user': user };
-    const removedOption = removed ? "/removed" : "";
-    const options = {
-      method: 'GET',
-      url: `${config.boUrl}/items${removedOption}?${qs.stringify(params)}`,
-      crossdomain : true,
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-    };
-  
-    try {
-      const response = await axios(options);
-      return computeAllItemsUtilityFields(response.data, user.language, characteristics);
-    }
-    catch(error) {
-      // console.error('fetchItemsFromServer error: ' , error);
-      throw error;
-    }
+  // No token, no fetchItemsFromServer!
+  const token = localStorage.getItem("accessToken");
+  if (!token) throw new Error({ error: "no token!" });
+  if (!user) throw new Error({ error: "no user!" });
+
+  const params = { access_token: token, user: user };
+  const removedOption = removed ? "/removed" : "";
+  const options = {
+    method: "GET",
+    url: `${config.boUrl}/items${removedOption}?${qs.stringify(params)}`,
+    crossdomain: true,
+    headers: { "content-type": "application/x-www-form-urlencoded" }
+  };
+
+  try {
+    const response = await axios(options);
+    return computeAllItemsUtilityFields(
+      response.data,
+      user.language,
+      characteristics
+    );
+  } catch (error) {
+    // console.error('fetchItemsFromServer error: ' , error);
+    throw error;
   }
-  
+}
 
-          
-  
-  async function addItemToServer(item, user) {
-    console.debug('|--- SERVER CALL ---|--- POST ---| Items.addItemToServer: ', item);
-    const data = { 'access_token': user.accessToken, ...item };
-    data.details = item.detailsArray.join();
-    const options = {
-      method: 'POST',
-      url: `${config.boUrl}/items`,
-      crossdomain : true,
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      data: qs.stringify(data),
-    };
+async function addItemToServer(item, user) {
+  console.debug(
+    "|--- SERVER CALL ---|--- POST ---| Items.addItemToServer: ",
+    item
+  );
+  const data = { access_token: user.accessToken, ...item };
+  data.details = item.detailsArray.join();
+  const options = {
+    method: "POST",
+    url: `${config.boUrl}/items`,
+    crossdomain: true,
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    data: qs.stringify(data)
+  };
 
-    try {
-      // console.debug('addItemToServer options: ' , options);
-      const response = await axios(options);
-      // console.debug('addItemToServer OK: ' , response.data);
-      return response.data;
-    } catch (error) {
-      console.error('register error: ' , error);
-      throw error;
-    }
+  try {
+    // console.debug('addItemToServer options: ' , options);
+    const response = await axios(options);
+    // console.debug('addItemToServer OK: ' , response.data);
+    return response.data;
+  } catch (error) {
+    console.error("register error: ", error);
+    throw error;
   }
-  
-  
-        
-  
-    
-  async function updateItemToServer(id, updates, user) {
-    console.debug('|--- SERVER CALL ---|--- PUT ---| Items.updateItemToServer: ', id, updates);
-    const data = { 'access_token': user.accessToken, ...updates };
-    const options = {
-      method: 'PUT',
-      url: `${config.boUrl}/items/${id}`,
-      crossdomain : true,
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      data: qs.stringify(data),
-    };
+}
 
-    try {
-      const response = await axios(options);
-      return response.data;
-    } catch (error) {
-      console.error('register error: ' , error);
-      throw error;
-    }
+async function updateItemToServer(id, updates, user) {
+  console.debug(
+    "|--- SERVER CALL ---|--- PUT ---| Items.updateItemToServer: ",
+    id,
+    updates
+  );
+  const data = { access_token: user.accessToken, ...updates };
+  const options = {
+    method: "PUT",
+    url: `${config.boUrl}/items/${id}`,
+    crossdomain: true,
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    data: qs.stringify(data)
+  };
+
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    console.error("register error: ", error);
+    throw error;
   }
-  
-  
-        
-  
-    
-  async function updatePictureItemToServer(id, picture, thumbnail, user) {
-    console.debug('|--- SERVER CALL ---|--- PUT ---| Items.updatePictureItemToServer: ', id);
+}
 
-    //
-    // Prepare the multipart parameters for the form-data
-    // with the id, the token and the 2 images (picture and thumbnail)
-    //
-    // (!) the name of the images MUST be setup here and will be used as filename
-    // on the server side
-    //
-    let formData = new FormData() // instantiate it
-    formData.set("id", id);
-    formData.set("access_token", user.accessToken);
-    formData.append("picture", picture, `${id}-picture-${Date.now()}.jpg`);
-    formData.append("picture", thumbnail, `${id}-thumbnail-${Date.now()}.jpg`);
+async function updatePictureItemToServer(id, picture, thumbnail, user) {
+  console.debug(
+    "|--- SERVER CALL ---|--- PUT ---| Items.updatePictureItemToServer: ",
+    id
+  );
 
-    const options = {
-      method: 'POST',
-      url: `${config.boUrl}/items/picture`,
-      crossdomain : true,
-      headers: { 'content-type': 'multipart/form-data' },
-      data: formData,
-    };
+  //
+  // Prepare the multipart parameters for the form-data
+  // with the id, the token and the 2 images (picture and thumbnail)
+  //
+  // (!) the name of the images MUST be setup here and will be used as filename
+  // on the server side
+  //
+  let formData = new FormData(); // instantiate it
+  formData.set("id", id);
+  formData.set("access_token", user.accessToken);
+  formData.append("picture", picture, `${id}-picture-${Date.now()}.jpg`);
+  formData.append("picture", thumbnail, `${id}-thumbnail-${Date.now()}.jpg`);
 
-    try {
-      const response = await axios(options);
-      return response.data;
-    } catch (error) {
-      console.error('updatePictureItemToServer error: ' , error);
-      throw error;
-    }
+  const options = {
+    method: "POST",
+    url: `${config.boUrl}/items/picture`,
+    crossdomain: true,
+    headers: { "content-type": "multipart/form-data" },
+    data: formData
+  };
+
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    console.error("updatePictureItemToServer error: ", error);
+    throw error;
   }
-  
-  
+}
 
-  async function removeItemOnServer(id, user, size) {
-    console.debug('|--- SERVER CALL ---|--- POST ---| Items.removeItemOnServer: ', id, size);
-    const data = { 'access_token': user.accessToken };
-    if(size && size !== '0') data['size'] = size;
-    const options = {
-      method: 'POST',
-      url: `${config.boUrl}/items/remove/${id}`,
-      crossdomain : true,
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      data: qs.stringify(data),
-    };
+async function removeItemOnServer(id, user, size) {
+  console.debug(
+    "|--- SERVER CALL ---|--- POST ---| Items.removeItemOnServer: ",
+    id,
+    size
+  );
+  const data = { access_token: user.accessToken };
+  if (size && size !== "0") data["size"] = size;
+  const options = {
+    method: "POST",
+    url: `${config.boUrl}/items/remove/${id}`,
+    crossdomain: true,
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    data: qs.stringify(data)
+  };
 
-    try {
-      const response = await axios(options);
-      return response.data;
-    } catch (error) {
-      console.error('removeItemOnServer error: ' , error);
-      throw error;
-    }
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    console.error("removeItemOnServer error: ", error);
+    throw error;
   }
-  
-  
+}
--- src/pages/Filters.jsx
+++ src/pages/Filters.jsx
@@ -1,70 +1,58 @@
-/* eslint-disable react-hooks/rules-of-hooks */ 
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState, useEffect } from "react";
-import { connect } from 'react-redux';
-import { itemsFilterActions } from '../_actions/itemsFilterActions';
+import { connect } from "react-redux";
+import { itemsFilterActions } from "../_actions/itemsFilterActions";
 import { injectIntl } from "react-intl";
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import { getIcon } from "../data/Icons"; 
-import { withStyles } from '@material-ui/core/styles';
-import TagManager from 'react-gtm-module'
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { getIcon } from "../data/Icons";
+import { withStyles } from "@material-ui/core/styles";
+import TagManager from "react-gtm-module";
 
 const FilterTabs = withStyles(theme => ({
   root: {
-    backgroundColor: theme.palette.primary.light,
-    borderBottom: '10px solid #e8e8e8',
+    backgroundColor: theme.palette.primary.light
   },
   indicator: {
     backgroundColor: theme.palette.primary.dark,
-    height:"3px",
-  },
+    height: "3px"
+  }
 }))(Tabs);
-
 
 const FilterTab = withStyles(theme => ({
   root: {
-    textTransform: 'none',
+    textTransform: "none",
     color: theme.palette.text.primary,
-    '&:hover': {
-      color: theme.palette.primary.dark,
+    "&:hover": {
+      color: theme.palette.primary.dark
     },
-    '&$selected': {
-      color: theme.palette.primary.dark,
+    "&$selected": {
+      color: theme.palette.primary.dark
     },
-    '&:focus': {
-      color: theme.palette.primary.dark,
-    },
+    "&:focus": {
+      color: theme.palette.primary.dark
+    }
   },
-  selected: {},
+  selected: {}
 }))(props => <Tab {...props} />);
 
-
-
-
-// WIP
-// see https://material-ui.com/components/tabs/#tabs
-// and https://material-ui.com/api/tab/
-
-
-function intFilters ({language, filter, categories, filterItems, intl}) {
-  
+function intFilters({ language, filter, categories, filterItems, intl }) {
   const [sortedCategories] = useState(
-    categories && categories.sort((a, b) => (a.name[language] > b.name[language]) ? 1 : -1)
+    categories &&
+      categories.sort((a, b) => (a.name[language] > b.name[language] ? 1 : -1))
   );
 
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   // Restore tab used in previous session:
   useEffect(() => {
-    console.log('Filters.useEffect() - should run only once!');
-    
-    if(!selectedCategory) {
-      let sel = localStorage.getItem('selectedCategory');
-      if(!sel) 
-        sel = sortedCategories[0].id2;
-      if(sel === 'removed')
-        sel = 'all';
-      setSelectedCategory( sel );
+    console.log("Filters.useEffect() - should run only once!");
+
+    if (!selectedCategory) {
+      let sel = localStorage.getItem("selectedCategory");
+      if (!sel) sel = sortedCategories[0].id2;
+      if (sel === "removed") sel = "all";
+      setSelectedCategory(sel);
       filterItems(sel);
 
       const tagManagerArgs = {
@@ -74,17 +62,14 @@ function intFilters ({language, filter, categories, filterItems, intl}) {
           action: "Init",
           value: sel
         }
-      }    
-      TagManager.initialize(tagManagerArgs);      
+      };
+      TagManager.initialize(tagManagerArgs);
     }
-  // },[]);  // ==> generates a warning on the console, but only way found to have it executed only once!
-  },[selectedCategory, sortedCategories, filterItems]);
+  }, [selectedCategory, sortedCategories, filterItems]);
 
-  if(!categories) return null;
-
+  if (!categories) return null;
 
   function handleChange(event, newValue) {
-
     const tagManagerArgs = {
       gtmId: "GTM-TFF4FK9",
       events: {
@@ -92,67 +77,66 @@ function intFilters ({language, filter, categories, filterItems, intl}) {
         action: "Change",
         value: newValue
       }
-    }    
-    TagManager.initialize(tagManagerArgs);  
+    };
+    TagManager.initialize(tagManagerArgs);
 
-    localStorage.setItem('selectedCategory', newValue);
-    setSelectedCategory( newValue );
+    localStorage.setItem("selectedCategory", newValue);
+    setSelectedCategory(newValue);
     filterItems(newValue);
   }
 
-
-  if(!selectedCategory) return null;
-  
+  if (!selectedCategory) return null;
 
   return (
     <React.Fragment>
-
       <FilterTabs
         value={selectedCategory}
         onChange={handleChange}
         variant="scrollable"
         scrollButtons="on"
         indicatorColor="primary"
-        // textColor="primary"
       >
         <FilterTab
-          id='filter.all'
-          key={'all'}
-          label={intl.formatMessage({id: "filter.all"})}
-          value={'all'}
-          icon={getIcon("all")} 
+          id="filter.all"
+          key={"all"}
+          label={intl.formatMessage({ id: "filter.all" })}
+          value={"all"}
+          icon={getIcon("all")}
+        />
+        <FilterTab
+          id="filter.latest"
+          key={"latest"}
+          label={intl.formatMessage({ id: "filter.latest" })}
+          value={"latest"}
+          icon={getIcon("latest")}
+        />
+        {sortedCategories.map(category => (
+          <FilterTab
+            id={"filter." + category.name["en"].toLowerCase()}
+            key={category.id2}
+            label={category.name[language]}
+            value={category.id2}
+            icon={getIcon("category" + category.id2)}
           />
+        ))}
         <FilterTab
-          id='filter.latest'
-          key={'latest'}
-          label={intl.formatMessage({id: "filter.latest"})}
-          value={'latest'}
-          icon={getIcon("latest")} 
+          id="filter.removed"
+          key={"removed"}
+          label={intl.formatMessage({ id: "filter.removed" })}
+          value={"removed"}
+          icon={getIcon("removed")}
         />
-        {sortedCategories.map(category => <FilterTab
-          id={'filter.' + category.name['en'].toLowerCase()}
-          key={category.id2}
-          label={category.name[language]}
-          value={category.id2}
-          icon={getIcon("category"+category.id2)} 
-        />)}
-        <FilterTab
-          id='filter.removed'
-          key={'removed'}
-          label={intl.formatMessage({id: "filter.removed"})}
-          value={'removed'}
-          icon={getIcon("removed")} 
-        />
-      </FilterTabs>      
-
+      </FilterTabs>
     </React.Fragment>
   );
 }
 
-
-
 function mapStateToProps(state) {
-  const { user: { language }, itemsFilter: { filter }, characteristics:{ categories } } = state;
+  const {
+    user: { language },
+    itemsFilter: { filter },
+    characteristics: { categories }
+  } = state;
   return {
     language,
     filter,
@@ -162,12 +146,13 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    filterItems: (filter) => dispatch(itemsFilterActions.filterItems(filter))
-  }
+    filterItems: filter => dispatch(itemsFilterActions.filterItems(filter))
+  };
 }
 
-const connectedFilters = connect(mapStateToProps, mapDispatchToProps)(intFilters);
+const connectedFilters = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(intFilters);
 
 export default injectIntl(connectedFilters);
-
-
